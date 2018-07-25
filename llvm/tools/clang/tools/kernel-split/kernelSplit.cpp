8b1928a709d798479442f99696e06a68b85733a4
Tyler Gobran <tylergobran@shaw.c>
2018-07-25 15:17:53 -0600

    Updated to only convery distribute parallel regions within a target region

--- llvm/tools/clang/tools/kernel-split/kernelSplit.cpp
+++ llvm/tools/clang/tools/kernel-split/kernelSplit.cpp
@@ -1,5 +1,7 @@
 #include "clang/AST/AST.h"
+#include "clang/AST/Stmt.h"
 #include "clang/AST/ASTConsumer.h"
+#include "clang/AST/ASTContext.h"
 #include "clang/AST/StmtOpenMP.h"
 #include "clang/AST/OpenMPClause.h"
 #include "clang/AST/RecursiveASTVisitor.h"
@@ -16,6 +18,7 @@
 #include "llvm/Support/CommandLine.h"
 
 #include <iostream>
+#include <iterator>
 
 using namespace clang;
 using namespace clang::driver;
@@ -25,28 +28,70 @@ using namespace llvm;
 class MyASTVisitor : public RecursiveASTVisitor<MyASTVisitor> {
     private:
         Rewriter &rewriter;
+        ASTContext &context;
+        SourceLocation targetRange;
+        SourceLocation distributeRange;
 
     public:
-        MyASTVisitor(Rewriter &R) : rewriter(R) {}
-       
+        MyASTVisitor(Rewriter &R, ASTContext &C) : rewriter(R) , context(C) {}
+        
         bool VisitOMPTargetTeamsDirective(OMPTargetTeamsDirective *d)
         {
-            
-            SourceLocation firstClause = d->clauses()[0]->getLocStart();
+            bool clauses = false;
             for(int i = 0; i < (int) d->getNumClauses(); ++i)
             {
                 OMPClause *c = d->clauses()[i];
-                if(c->getClauseKind() != OMPC_map)
-                    rewriter.RemoveText(SourceRange(c->getLocStart(),c->getLocEnd()));
+                if(!c->isImplicit())
+                {
+                    clauses = true;
+                    if(c->getClauseKind() != OMPC_map)
+                        rewriter.RemoveText(SourceRange(c->getLocStart(),c->getLocEnd()));
+                }
             }
-            rewriter.ReplaceText(SourceRange(d->getLocStart(),firstClause.getLocWithOffset(-1)), "omp target data");
+
+            if(clauses)
+                rewriter.ReplaceText(SourceRange(d->getLocStart(),d->clauses()[0]->getLocStart().getLocWithOffset(-1)), "omp target data");
+            else
+                rewriter.ReplaceText(SourceRange(d->getLocStart(),d->getLocEnd()),"omp target data");
+
+
+            const Stmt *parent = context.getParents(*d)[0].get<Stmt>();
+            bool next = false;
+            for(ConstStmtIterator iter = parent->child_begin(); iter!=parent->child_end(); iter++)
+            {
+                if(next)
+                {
+                    targetRange = iter->getLocStart().getLocWithOffset(-1);
+                    break;
+                }     
+                if(iter->getLocStart() == d->getLocStart())
+                    next = true;    
+            }
+
             return true;
         }
 
         bool VisitOMPDistributeParallelForDirective(OMPDistributeParallelForDirective *d)
         {
-            rewriter.InsertText(d->getLocStart().getLocWithOffset(4), "target teams ");
-            rewriter.InsertText(d->getLocEnd(), " thread_limit(128)");
+            if(rewriter.getSourceMgr().isBeforeInTranslationUnit(d->getLocStart(),targetRange))
+            {
+                rewriter.InsertText(d->getLocStart().getLocWithOffset(4), "target teams ");
+                rewriter.InsertText(d->getLocEnd(), " thread_limit(128)");
+
+                const Stmt *parent = context.getParents(*d)[0].get<Stmt>();
+                bool next = false;
+                for(ConstStmtIterator iter = parent->child_begin(); iter!=parent->child_end(); iter++)
+                {
+                    std::cout << iter->getStmtClassName() << std::endl;
+                    if(next)
+                    {
+                        distributeRange = iter->getLocStart().getLocWithOffset(-1);
+                        break;
+                    }     
+                    if(iter->getLocStart() == d->getLocStart())
+                        next = true;    
+                }
+            }
             return true;
         }
 };
@@ -56,7 +101,7 @@ class MyASTConsumer : public ASTConsumer {
         MyASTVisitor visitor;
 
     public:
-        MyASTConsumer(Rewriter &R) : visitor(R) {}
+        MyASTConsumer(Rewriter &R, ASTContext &C) : visitor(R, C) {}
         
         bool HandleTopLevelDecl(DeclGroupRef DR) override {
             for(DeclGroupRef::iterator b = DR.begin(), e = DR.end(); b !=e; ++b) {
@@ -78,11 +123,11 @@ class MyFrontendAction : public ASTFrontendAction {
             llvm::errs() << "** EndSourceFileAction for: " << SM.getFileEntryForID(SM.getMainFileID())->getName() << "\n";
             rewriter.getEditBuffer(SM.getMainFileID()).write(llvm::outs());
         }
-        
+         
         std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI, StringRef file) override {
             llvm::errs() << "** Creating AST consumer for: " << file << "\n";
             rewriter.setSourceMgr(CI.getSourceManager(), CI.getLangOpts());
-            return llvm::make_unique<MyASTConsumer>(rewriter);
+            return llvm::make_unique<MyASTConsumer>(rewriter, CI.getASTContext());
         }
 };    
 		
