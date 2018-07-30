f13ccb936bc531628dc68532b4ffc1924ef55343
Tyler Gobran <tylergobran@shaw.c>
2018-07-30 16:46:50 -0600

    Updated code with comments and added handling for nested parallel statements

--- llvm/tools/clang/tools/kernel-split/kernelSplit.cpp
+++ llvm/tools/clang/tools/kernel-split/kernelSplit.cpp
@@ -24,6 +24,10 @@
 #include <iostream>
 #include <iterator>
 
+#define PRAGMA_SIZE 8
+#define DEFAULT_THREAD_LIMIT 128
+#define DEFAULT_BLOCK_NUMBER 448
+
 using namespace llvm;
 using namespace clang;
 using namespace clang::driver;
@@ -33,9 +37,13 @@ class MyASTVisitor : public RecursiveASTVisitor<MyASTVisitor> {
     private:
         Rewriter &rewriter;
         ASTContext &context;
+
+        //Variables used for the basic tree traversal and application of the splitting
         std::stringstream targetClauses;
         std::stringstream mapClauses;
         bool inTarget = false;
+        bool inTargetData = false;
+        bool hasParallel = false;
         bool firstParallel = false;
         bool findEnd = false;
         SourceLocation endLoc;
@@ -45,32 +53,39 @@ class MyASTVisitor : public RecursiveASTVisitor<MyASTVisitor> {
         int stmtDepth;
         int parallelDepth;
 
+        //Variables used for the application of custom grid geometry
+        int threadLimit = DEFAULT_THREAD_LIMIT;
+        int blockNum = DEFAULT_BLOCK_NUMBER;
+        
     public:
         MyASTVisitor(Rewriter &R, ASTContext &C) : rewriter(R) , context(C) {}
 
+        //Removes the right bracket corresponding to a left bracket found right before loc
         void removeRightBrkt(SourceLocation loc) {
             int lefts = 0;
             SourceLocation tokLoc;
             Token currTok;
-            while(true) {
+
+            //Iterates through the original source code tokens until a corresponding right parantheses is found
+            while(lefts > -1) {
                 tokLoc = Lexer::GetBeginningOfToken(loc,rewriter.getSourceMgr(), rewriter.getLangOpts());
                 bool noToken = Lexer::getRawToken(tokLoc, currTok, rewriter.getSourceMgr(), rewriter.getLangOpts()); 
                 loc = loc.getLocWithOffset(1);
                 if(!noToken) {
                     if (currTok.getKind() == tok::l_brace)
                         lefts++;
-                    else if (currTok.getKind() == tok::r_brace) {
-                        if(lefts != 0)
-                            lefts--;
-                        else
-                            break;
-                    }
+                    else if (currTok.getKind() == tok::r_brace)
+                        lefts--;
                 }
             }
+
+            //Removes that right parantheses from the source code
             rewriter.RemoveText(SourceRange(tokLoc,loc));
+
             return;
         }
         
+        //Adds a right bracket at the end of the given subtree by finding the furthers end location within and adding a right bracket there
         void addRightBrkt(Stmt *s) {
             endLoc = s->getLocEnd();
             findEnd = true;
@@ -79,109 +94,188 @@ class MyASTVisitor : public RecursiveASTVisitor<MyASTVisitor> {
             rewriter.InsertText(endLoc.getLocWithOffset(1), "}");
             return;
         }
- 
+
+        //Finds the location of the right parantheses of a given statement starting at loc
+        SourceLocation findRightParenth(SourceLocation loc) {
+            int lefts = 0;
+            SourceLocation tokLoc;
+            Token currTok;
+            bool within = false;
+
+            //Iterates through the original source code tokens until a corresponding right parantheses is found
+            while(lefts > 0 or !within) {
+                tokLoc = Lexer::GetBeginningOfToken(loc,rewriter.getSourceMgr(), rewriter.getLangOpts());
+                bool noToken = Lexer::getRawToken(tokLoc, currTok, rewriter.getSourceMgr(), rewriter.getLangOpts());
+                loc = loc.getLocWithOffset(1);
+                if(!noToken) {
+                    if (currTok.getKind() == tok::l_paren) {
+                        lefts++;
+                        within = true;
+                    }
+                    else if (currTok.getKind() == tok::r_paren)
+                        lefts--;
+                }
+            }
+            return loc;
+        }
+
+        //Function called by the visitor upon finding a stmt
         bool VisitStmt(Stmt *s) {
+
+            //Used when calculating the end point of a subtree, compares the current statements end location to the furthest one calculated
             if(findEnd and rewriter.getSourceMgr().isBeforeInTranslationUnit(endLoc,s->getLocEnd()))
                 endLoc = s->getLocEnd();
+
             return true;
         }
         
+        //Function called by the visitor before traversing down an OpenMP "target teams" directive
         bool TraverseOMPTargetTeamsDirective(OMPTargetTeamsDirective *d) {
+
+            //Performs a traversal of the subtree to calculate the highest depth parallel directive
             stmtDepth = 0;
             parallelDepth = -1;
             findParallelDepth = true;
             RecursiveASTVisitor<MyASTVisitor>::TraverseOMPTargetTeamsDirective(d);
             findParallelDepth = false;
+
+            //Performs the actual full traversal of the subtree with the previously calculated information
             RecursiveASTVisitor<MyASTVisitor>::TraverseOMPTargetTeamsDirective(d);
             inTarget = false;
             firstParallel = false;
+            hasParallel = false;
+
+            return true;
+        }
+        
+        //Function called by the visitor before traversing down an OpenMP "target data" directive 
+        bool TraverseOMPTargetDataDirective(OMPTargetDataDirective *d) {
+            inTargetData = true;
+            RecursiveASTVisitor<MyASTVisitor>::TraverseOMPTargetDataDirective(d);
+            inTargetData = false;
             return true;
         }
 
-        bool TraverseStmt(Stmt *s, DataRecursionQueue *Queue=nullptr) {
-            stmtDepth++;
-            RecursiveASTVisitor<MyASTVisitor>::TraverseStmt(s,Queue);
-            stmtDepth--;
+        //Function called by the visitor before traversing down a compound stmt (for, while, if, do, switch statments generally)
+        bool TraverseCompoundStmt(CompoundStmt *s) {
+            ++stmtDepth;
+            RecursiveASTVisitor<MyASTVisitor>::TraverseCompoundStmt(s);
+            --stmtDepth;
             return true;
         }
- 
+
+        //Function called by the visitor upon finding an OpenMP "target teams" directive
         bool VisitOMPTargetTeamsDirective(OMPTargetTeamsDirective *d) {
+
+            //Prevents changes when just searching for parallel depth
             if(findParallelDepth)
                 return true;
+            
+            //Prevents changes to a target region that is only serial but is in a target data region
+            if(!hasParallel and inTargetData)
+                return true;            
+
+            //Resets variables containing target region information
+            inTarget = true;
             numParallel = 0;
             curParallel = 0;
             targetClauses.str("");
             mapClauses.str("");
+
+            //Finds all clauses for the given directive and saves then for later use
             for(int i = 0; i < (int) d->getNumClauses(); ++i) {
                 OMPClause *c = d->clauses()[i];
                 if(!c->isImplicit()) {
+                        SourceLocation start = c->getLocStart();
+                        SourceLocation end = findRightParenth(start);
                         if(c->getClauseKind() != OMPC_map)
-                            targetClauses << rewriter.getRewrittenText(SourceRange(c->getLocStart(),c->getLocEnd()));
+                            targetClauses << rewriter.getRewrittenText(SourceRange(start,end)) << " ";
                         else
-                            mapClauses << rewriter.getRewrittenText(SourceRange(c->getLocStart(),c->getLocEnd()));
+                            mapClauses << rewriter.getRewrittenText(SourceRange(start,end)) << " ";
                 }
             }
+
+            //Removes the original target teams statement and any related brackets in the original source code 
             SourceLocation leftBrkt = Lexer::findLocationAfterToken(d->getLocEnd(),tok::l_brace,rewriter.getSourceMgr(),rewriter.getLangOpts(),true);
             if(leftBrkt.isValid()) {
-                rewriter.RemoveText(SourceRange(d->getLocStart().getLocWithOffset(-8),leftBrkt));
+                rewriter.RemoveText(SourceRange(d->getLocStart().getLocWithOffset(-PRAGMA_SIZE),leftBrkt));
                 removeRightBrkt(leftBrkt);
             }
             else
-                rewriter.RemoveText(SourceRange(d->getLocStart().getLocWithOffset(-8),leftBrkt));
-            inTarget = true;
+                rewriter.RemoveText(SourceRange(d->getLocStart().getLocWithOffset(-PRAGMA_SIZE),d->getLocEnd()));
+
             return true;
         }
 
+        //Function called by the visitor upon finding an OpenMP "distribute parallel for" directive
         bool VisitOMPDistributeParallelForDirective(OMPDistributeParallelForDirective *d) {
-            std::cout << stmtDepth << std::endl;
-            if(findParallelDepth and (stmtDepth < parallelDepth or parallelDepth == -1))
+            
+            hasParallel = true;
+ 
+            //Compares the given directives depth to the highest one
+            if (findParallelDepth and (stmtDepth < parallelDepth or parallelDepth == -1))
                 parallelDepth = stmtDepth;
-            else if(inTarget and !findEnd and stmtDepth == parallelDepth) {
-               curParallel++;
-               if(!firstParallel) {
-                   firstParallel = true;
-                   rewriter.InsertText(d->getLocStart().getLocWithOffset(-8), "#pragma omp target data " + mapClauses.str() + "{\n", true, true);
-                   const Stmt *parent = context.getParents(*d)[0].get<Stmt>();
-                   int i = 0, fp = -1, lp = -1;
-                   for(ConstStmtIterator iter = parent->child_begin(); iter!=parent->child_end();iter++) {
-                       if(isa<OMPDistributeParallelForDirective>(*iter)) {
-                           numParallel++;
-                           if(fp == -1)
-                               fp = i;
-                           lp = i;
-                       }
-                       i++;
-                   }
 
-                   bool previousSerial = false;
-                   bool previousParallel = false;
-                   i = 0;
-                   for(ConstStmtIterator iter = parent->child_begin(); iter!=parent->child_end(); iter++) {  
-                       if(isa<OMPDistributeParallelForDirective>(*iter)) {
-                           if(previousSerial and fp != i)
-                               rewriter.InsertText(iter->getLocStart().getLocWithOffset(-8), "}\n", true, true);
-                           previousParallel = true;
-                           previousSerial = false;
-                       }
-                       else {
-                           if(previousParallel and lp > i)
-                               rewriter.InsertText(iter->getLocStart(), "#pragma omp target teams {\n" + targetClauses.str(), true, true);
-                           previousParallel = false;
-                           previousSerial = true;
-                       }
-                   i++;
-                   }
-                   
-               }
-               if(numParallel == curParallel)
-                   addRightBrkt(d);
-               rewriter.InsertText(d->getLocStart().getLocWithOffset(4), "target teams ");
-               rewriter.InsertText(d->getLocEnd()," " + targetClauses.str());
+            //Handles the transformation of the directive that it at the highest depth
+            else if (inTarget and !findEnd and stmtDepth == parallelDepth) {
+                curParallel++;
+               
+                //Calculates the number of parallel regions at the given depth and handles all serial regions
+                if (!firstParallel) {
+                    firstParallel = true;
+                    
+                    //Adds an encompassing target data region starting at the first parallel region
+                    if (!inTargetData)
+                        rewriter.InsertText(d->getLocStart().getLocWithOffset(-8), "#pragma omp target data "+mapClauses.str()+"\n{\n", true, true);
+                    
+                    //Finds all other stmts at the same depth as the directive and counts the number of parallel regions and their positions
+                    const Stmt *parent = context.getParents(*d)[0].get<Stmt>();
+                    int i = 0, fp = -1, lp = -1;
+                    for (ConstStmtIterator iter = parent->child_begin(); iter!=parent->child_end(); ++iter, ++i) {
+                        if (isa<OMPDistributeParallelForDirective>(*iter)) {
+                            numParallel++;
+                            if (fp == -1)
+                                fp = i;
+                            lp = i;
+                        }
+                    }
+
+                    //Using the previous information, blocks of all serial inbetween parallel ones into their own seperate target regions 
+                    bool prevPrl = false;
+                    bool prevSrl = false;
+                    i = 0;
+                    for(ConstStmtIterator iter = parent->child_begin(); iter!=parent->child_end(); ++iter, ++i) {  
+                        if(isa<OMPDistributeParallelForDirective>(*iter)) {
+                            if(prevSrl and fp != i)
+                                rewriter.InsertText(iter->getLocStart().getLocWithOffset(-8), "}\n", true, true);
+                            prevPrl = true;
+                            prevSrl = false;
+                        }
+                        else {
+                            if(prevPrl and lp > i)
+                                rewriter.InsertText(iter->getLocStart(), "#pragma omp target teams "+targetClauses.str()+"\n{\n", true, true);
+                            prevPrl = false;
+                            prevSrl = true;
+                        }
+                    }                  
+                }
+
+                //Adds a closing right bracket after the last parallel region
+                if(numParallel == curParallel and !inTargetData)
+                    addRightBrkt(d);
+
+                //Converts the directive into its own target region
+                rewriter.InsertText(d->getLocStart().getLocWithOffset(4), "target teams ");
+                if(inTargetData)
+                    rewriter.InsertText(d->getLocEnd()," " + mapClauses.str() + targetClauses.str());
+                else
+                    rewriter.InsertText(d->getLocEnd()," " + targetClauses.str());
             }
             return true;
         }
 };
 
+//Consumes the created AST by the compiler, with each found function initiates a traversal from which the recursive visitor works
 class MyASTConsumer : public ASTConsumer {
     private:
         MyASTVisitor visitor;
@@ -197,6 +291,7 @@ class MyASTConsumer : public ASTConsumer {
         }
 };
 
+//The front end of the tool, which creates the AST consumer for the given file and produces the rewritten code upon tool completion
 class MyFrontendAction : public ASTFrontendAction {
     private:
         Rewriter rewriter;
@@ -217,12 +312,12 @@ class MyFrontendAction : public ASTFrontendAction {
         }
 };    
 		
-//Tool help menu options
+//Defines the tools help menu options
 static llvm::cl::OptionCategory toolCategory("my-tool options");
 static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
 static cl::extrahelp MoreHelp("\nMore help text...\n");
 
-//Parse options and apply tool to defined files
+//Parses any given options and then applies the tool to the defined files through the frontend action
 int main(int argc, const char **argv) {
     CommonOptionsParser op(argc, argv, toolCategory);
     ClangTool Tool(op.getCompilations(),op.getSourcePathList());
